8215041df2619be29bdb5b47a831bd22c173813d
PILsystem <amitece151@gmail.com>
2024-09-02 23:24:03 +0530

    all file are add 31 aug 2024 Task on object

---
+++ src/ex_31082024/Lab_0041.py
@@ -0,0 +1,32 @@
+class person():
+    # Atribute
+    name = None
+    email = None
+    age = None
+    Address = None
+
+    # Bhihaviour
+
+
+def walk(self):
+    print(" i am walking")
+
+
+def sleep(self):
+    return "sleeping"
+
+
+def eat(self, name):
+    print("eating a mango")
+
+
+def talk(self):
+    print("i can talk")
+    return ("i can talk")
+
+
+# create a object
+amit = person()
+amit.name = "Amit Kumar"
+print(amit.name)
+amit.talk()
---
+++ src/ex_31082024/Lab_0042.py
@@ -0,0 +1,15 @@
+
+
+
+class Dog3():
+    name = None
+    Breed = None
+    color = None
+
+    def braek(self):
+        print("dog is break")
+
+    def sleep(self):
+        print("dog is sleeping")
+dog4= Dog3()
+
